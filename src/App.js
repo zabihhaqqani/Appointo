533ae4dda883a3fe1c05a560d152ffe27aa1c89c
zabihhaqqani <mdzabihhaqqani@gmail.com>
2024-01-21 09:54:16 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import LeftBands from "./assets/bands/LeftBands";
+import RightBands from "./assets/bands/RightBands";
+import Navbar from "./components/navbar/Navbar";
+import CalendarView from "./pages/calendarView/CalendarView";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">Appointo App</header>
+    <div>
+      <Navbar />
+      <RightBands />
+      <LeftBands />
+      <div className="App">
+        <CalendarView />
+      </div>
     </div>
   );
 };
